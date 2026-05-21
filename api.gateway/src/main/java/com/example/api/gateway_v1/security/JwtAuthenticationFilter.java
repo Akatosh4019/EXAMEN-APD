493150cc2ca3a6d0a86bb436a15681daead93748
Samuel <samuelvalencia3019@gmail.com>
2026-05-20 23:48:05 -0500

    Implementa JWT y validacion en api-gateway

--- api.gateway/src/main/java/com/example/api/gateway_v1/security/JwtAuthenticationFilter.java
+++ api.gateway/src/main/java/com/example/api/gateway_v1/security/JwtAuthenticationFilter.java
@@ -1,4 +1,55 @@
 package com.example.api.gateway_v1.security;
 
-public class JwtAuthenticationFilter {
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtAuthenticationFilter implements GlobalFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        String path = exchange.getRequest().getURI().getPath();
+
+        if (path.startsWith("/api/auth") || path.startsWith("/auth")) {
+            return chain.filter(exchange);
+        }
+
+        String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        try {
+            String token = authHeader.substring(7);
+            Claims claims = jwtUtil.validarToken(token);
+
+            String username = claims.getSubject();
+            String rol = claims.get("groups").toString();
+
+            ServerWebExchange modifiedExchange = exchange.mutate()
+                    .request(builder -> builder
+                            .header("X-User-Name", username)
+                            .header("X-User-Role", rol)
+                    )
+                    .build();
+
+            return chain.filter(modifiedExchange);
+
+        } catch (Exception e) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+    }
 }
